d3b1a16291a8a69ea418af9e55617e2076d16caa
Thuan Nguyen Phuong <nguyenphuongthuan95@gmail.com>
2019-03-16 12:31:26 +0700

    Eslint and Prettier config

---
+++ frontend/pages/test.js
@@ -0,0 +1,16 @@
+import Head from '../components/head';
+
+function Test() {
+  return (
+    <div>
+      <p>
+        <Head title="Test" />
+        Welcome to Test!
+      </p>
+
+      <div>Trying to test </div>
+    </div>
+  );
+}
+
+export default Test;
